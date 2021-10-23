50047b156d7e0acde7f303b3dc46db7d826c5790
Tiago Melo Juca <tiagomelojuca@gmail.com>
2021-10-23 17:30:31 -0300

    generate debug symbols when running compile script

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-cmake -G "MinGW Makefiles" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -S . -B build
+cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -S . -B build
 cmake --build build
 cmake --install build
