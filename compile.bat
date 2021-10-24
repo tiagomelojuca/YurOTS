aa74a9c30897d31141d49650fc5336d2a34f6977
Tiago Melo Juca <tiagomelojuca@gmail.com>
2021-10-24 17:30:14 -0300

    update compile script to use gcc instead of clang

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -S . -B build
+cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -S . -B build
 cmake --build build
 cmake --install build
